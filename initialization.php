58c23f9246cfe874d882c5df719808c2dd0c8e0d
Your Name <you@example.com>
2024-09-22 08:17:58 +0000

    first commit

---
+++ initialization.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Hexa PR Wire - Distributor
+Description: Basic tools for optimization, performance, and debugging on Hexa-based web systems.
+Author: Michael Peres
+Plugin URI: https://github.com/mikeyperes/hws-base-tools
+Version: 1
+Author URI: https://michaelperes.com
+GitHub Plugin URI: https://github.com/mikeyperes/hws-base-tools/
+GitHub Branch: main 
+*/          
+namespace hpr_distributor ;
+?>
